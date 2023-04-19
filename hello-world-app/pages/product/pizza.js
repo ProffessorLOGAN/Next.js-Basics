d78624a3e78a7480c1ea2279cf717eedcf8bd45d
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2023-04-19 21:25:16 +0530

    Dynamic routes in Next js

---
+++ hello-world-app/pages/product/pizza.js
@@ -0,0 +1,12 @@
+function Pizza() {
+    return (<>
+        <h1>Landing Page For Pizza</h1>
+
+        <h4><strong>Note :</strong> <i>if product page is available then product name  page is rendered,not the product id </i></h4>
+        <h4><strong>Note :</strong> <i>if product page is not available then product id  page is rendered </i></h4>
+    </>
+
+    )
+}
+
+export default Pizza;
