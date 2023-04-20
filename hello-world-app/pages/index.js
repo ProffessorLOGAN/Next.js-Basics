cc2e825a913d8c654b2f7ac0a22dd8791bfd7f8d
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2023-04-20 11:49:50 +0530

    Link Component navigation in Nextjs

--- hello-world-app/pages/index.js
+++ hello-world-app/pages/index.js
@@ -1,6 +1,6 @@
+import Link from 'next/link'
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,10 +13,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <h1>Hello world</h1>
-        <h1 >Tansen Keshri</h1>
-      </main>
+      <div>
+        <h1> Home Page </h1>
+
+        <Link href='/blog'>
+          Blog
+        </Link>
+        <Link href='/product'>
+          Products
+        </Link>
+      </div>
     </>
   )
 }
--- hello-world-app/pages/product/index.js
+++ hello-world-app/pages/product/index.js
@@ -1,10 +1,32 @@
-function ProductList() {
+import Link from "next/link";
+
+function ProductList({ productId = 100 }) {
     return (
         <>
+            <Link href='/'>
+                Home
+            </Link>
             <h1>Product List</h1>
-            <h2>Product 1</h2>
-            <h2>Product 2</h2>
-            <h2>Product 3</h2>
+            <h2>
+                <Link href='/product/1'>
+                    Product 1
+                </Link>
+            </h2>
+            <h2>
+                <Link href='/product/2'>
+                    Product 2
+                </Link>
+            </h2>
+            <h2>
+                <Link href='/product/3'>
+                    Product 3
+                </Link>
+            </h2>
+            <h2>
+                <Link href={`/product/${productId}`}>
+                    Product {productId}
+                </Link>
+            </h2>
         </>
     )
 }
